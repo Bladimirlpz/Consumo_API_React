0514905bd4686d6dc119f5688d11356304632449
Bladimirlpz <bladimir.lopez.p@gmail.com>
2024-01-14 22:02:02 -0300

    clase a seccion de los resultados

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -3,8 +3,7 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import { consultarApi } from "../utilities/base-dato";
 import Table from "react-bootstrap/esm/Table";
-import "./Buscador.css"
-
+import "./Buscador.css";
 
 function Buscador() {
   const [farmacia, setFarmacia] = useState("");
@@ -19,20 +18,33 @@ function Buscador() {
   const inputFarmacia = (e) => {
     setFarmacia(e.target.value);
   };
-  const filtroFarmacia = data.filter((farmacias) => farmacias.local_nombre.toLowerCase() === farmacia.toLowerCase());
+  const filtroFarmacia = data.filter(
+    (farmacias) =>
+      farmacias.local_nombre.toLowerCase() === farmacia.toLowerCase()
+  );
 
   // INPUT Y FILTRADO POR COMUNA
-  const inputComuna = (e) => { 
-    setComuna(e.target.value)
+  const inputComuna = (e) => {
+    setComuna(e.target.value);
   };
-  const filtroComuna = data.filter((farmacias) => farmacias.comuna_nombre.toLowerCase() === comuna.toLowerCase());
+  const filtroComuna = data.filter(
+    (farmacias) =>
+      farmacias.comuna_nombre.toLowerCase() === comuna.toLowerCase()
+  );
 
   return (
     <>
       <section className="seccion-buscador">
         <h1>Buscador</h1>
-        <InputGroup size="sm" className="mb-2 input-farmcia" value={farmacia} onChange={inputFarmacia}>
-          <InputGroup.Text style={{ width: '5rem' }} id="inputGroup-sizing-sm">Farmacia</InputGroup.Text>
+        <InputGroup
+          size="sm"
+          className="mb-2 input-farmcia"
+          value={farmacia}
+          onChange={inputFarmacia}
+        >
+          <InputGroup.Text style={{ width: "5rem" }} id="inputGroup-sizing-sm">
+            Farmacia
+          </InputGroup.Text>
           <Form.Control
             placeholder="Ingrese nombre de la Farmacia"
             type="text"
@@ -40,8 +52,15 @@ function Buscador() {
             aria-describedby="inputGroup-sizing-sm"
           />
         </InputGroup>
-        <InputGroup size="sm" className="mb-2 input-comuna" value={comuna} onChange={inputComuna}>
-          <InputGroup.Text style={{ width: '5rem' }} id="inputGroup-sizing-sm">Comuna</InputGroup.Text>
+        <InputGroup
+          size="sm"
+          className="mb-2 input-comuna"
+          value={comuna}
+          onChange={inputComuna}
+        >
+          <InputGroup.Text style={{ width: "5rem" }} id="inputGroup-sizing-sm">
+            Comuna
+          </InputGroup.Text>
           <Form.Control
             placeholder="Ingrese la Comuna"
             type="text"
@@ -50,66 +69,68 @@ function Buscador() {
           />
         </InputGroup>
       </section>
-      {farmacia? 
-      <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th>Farmacia</th>
-          <th>Region</th>
-          <th>Comuna</th>
-          <th>Direccion</th>
-          <th>Apertura</th>
-          <th>Cierre</th>
-          <th>Telefono</th>
-          <th>Turno Dia</th>
-        </tr>
-      </thead>
-      <tbody>
-        {filtroFarmacia.map((farmacia) => (
-          <tr key={farmacia.local_id}>
-            <td>{farmacia.local_nombre}</td>
-            <td>{farmacia.fk_region}</td>
-            <td>{farmacia.comuna_nombre}</td>
-            <td>{farmacia.local_direccion}</td>
-            <td>{farmacia.funcionamiento_hora_apertura}</td>
-            <td>{farmacia.funcionamiento_hora_cierre}</td>
-            <td>{farmacia.local_telefono}</td>
-            <td>{farmacia.funcionamiento_dia}</td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
-      : null}
-      {comuna? 
-      <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th>Farmacia</th>
-          <th>Region</th>
-          <th>Comuna</th>
-          <th>Direccion</th>
-          <th>Apertura</th>
-          <th>Cierre</th>
-          <th>Telefono</th>
-          <th>Turno Dia</th>
-        </tr>
-      </thead>
-      <tbody>
-        {filtroComuna.map((farmacia) => (
-          <tr key={farmacia.local_id}>
-            <td>{farmacia.local_nombre}</td>
-            <td>{farmacia.fk_region}</td>
-            <td>{farmacia.comuna_nombre}</td>
-            <td>{farmacia.local_direccion}</td>
-            <td>{farmacia.funcionamiento_hora_apertura}</td>
-            <td>{farmacia.funcionamiento_hora_cierre}</td>
-            <td>{farmacia.local_telefono}</td>
-            <td>{farmacia.funcionamiento_dia}</td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
-      : null}
+      <section className="resultado-buscador">
+        {farmacia ? (
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>Farmacia</th>
+                <th>Region</th>
+                <th>Comuna</th>
+                <th>Direccion</th>
+                <th>Apertura</th>
+                <th>Cierre</th>
+                <th>Telefono</th>
+                <th>Turno Dia</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filtroFarmacia.map((farmacia) => (
+                <tr key={farmacia.local_id}>
+                  <td>{farmacia.local_nombre}</td>
+                  <td>{farmacia.fk_region}</td>
+                  <td>{farmacia.comuna_nombre}</td>
+                  <td>{farmacia.local_direccion}</td>
+                  <td>{farmacia.funcionamiento_hora_apertura}</td>
+                  <td>{farmacia.funcionamiento_hora_cierre}</td>
+                  <td>{farmacia.local_telefono}</td>
+                  <td>{farmacia.funcionamiento_dia}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        ) : null}
+        {comuna ? (
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>Farmacia</th>
+                <th>Region</th>
+                <th>Comuna</th>
+                <th>Direccion</th>
+                <th>Apertura</th>
+                <th>Cierre</th>
+                <th>Telefono</th>
+                <th>Turno Dia</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filtroComuna.map((farmacia) => (
+                <tr key={farmacia.local_id}>
+                  <td>{farmacia.local_nombre}</td>
+                  <td>{farmacia.fk_region}</td>
+                  <td>{farmacia.comuna_nombre}</td>
+                  <td>{farmacia.local_direccion}</td>
+                  <td>{farmacia.funcionamiento_hora_apertura}</td>
+                  <td>{farmacia.funcionamiento_hora_cierre}</td>
+                  <td>{farmacia.local_telefono}</td>
+                  <td>{farmacia.funcionamiento_dia}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        ) : null}
+      </section>
     </>
   );
 }
